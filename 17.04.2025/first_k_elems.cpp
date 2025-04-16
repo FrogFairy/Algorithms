a0900a9e6f68a0f29a4a6c75366529ea2de45bde
Daria Novoselova <novdasha535@yandex.ru>
2025-04-17 00:09:30 +0300

    added solution for "first k elements" task

---
+++ 17.04.2025/first_k_elems.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
+
+void add_elem(std::vector<int>& heap, int elem, int k)
+{
+    if (heap.size() < k)
+    {
+        heap.push_back(elem);
+        std::push_heap(heap.begin(), heap.end());
+    }
+    else if (elem < heap[0])
+    {
+        std::pop_heap(heap.begin(), heap.end());
+        heap.pop_back();
+
+        heap.push_back(elem);
+        std::push_heap(heap.begin(), heap.end());
+    }
+}
+
+std::ostream& operator<< (std::ostream& os, std::vector<int>& vec)
+{
+    for (int i = 0; i < vec.size(); ++i)
+        os << vec[i] << std::endl;
+    return os;
+}
+
+int main()
+{
+    int n, k;
+    std::cin >> n >> k;
+    std::vector<int> result;
+    std::make_heap(result.begin(), result.end());
+    for (int i = 0; i < n; ++i)
+    {
+        int val;
+        std::cin >> val;
+        add_elem(result, val, k);
+    }
+
+    std::sort(result.begin(), result.end());
+    std::cout << result << std::endl;
+}
